b60487c9e5e4973deb6f39396dc4aaae146e417a
Gabriel Cardoso <artesilksjc@hotmail.com>
2022-08-23 09:56:09 -0300

    Dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,7 +91,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: (process.env.PORT || 8080),
+        port: (process.env.PORT || 9000),
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35729
